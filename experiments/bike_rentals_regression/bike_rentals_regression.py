eb5de9c3824c5f8da75bfbeee40076cf6556842b
Afek <afekilayadler@gmail.com>
2020-05-03 15:07:42 +0300

    added ntrees, nleaves and rmse

--- experiments/bike_rentals_regression/bike_rentals_regression.py
+++ experiments/bike_rentals_regression/bike_rentals_regression.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from experiments.default_config import RESULTS_DIR, VAL_RATIO, MAX_DEPTH, N_ESTIMATORS, LEARNING_RATE, GBM_REGRESSORS
 from experiments.utils import make_dirs, transform_categorical_features
 import os
+from pathlib import Path
+
 
 def get_x_y():
     def preprocess_df(df):
@@ -16,12 +18,12 @@ def get_x_y():
         df['season'] = df['season'].astype('category')
         df['workingday'] = df['workingday'].astype('category')
         df['weather'] = df['weather'].astype('category')
-        X =  df[['datetime', 'season', 'holiday', 'workingday', 'weather', 'temp',
-       'atemp', 'humidity', 'windspeed']]
+        X = df[['datetime', 'season', 'holiday', 'workingday', 'weather', 'temp',
+                'atemp', 'humidity', 'windspeed']]
         y = df['count']
         return X, y
-
-    train = read_csv(r"C:\Users\afeki\OneDrive\Desktop\code\Final\datasets\bike_rental_regression\train.csv")
+    project_root =  Path(__file__).parent.parent.parent
+    train = read_csv(project_root / 'datasets/bike_rental_regression/train.csv')
     X, y = preprocess_df(train)
     return X, y
 
@@ -38,27 +40,31 @@ def worker(model_name, variant):
     results = {'model': F"{model_name}_{variant}"}
     X_train, X_test = transform_categorical_features(X_train, X_test, y_train, variant)
     model = GBM_REGRESSORS[model_name](variant, X.dtypes, max_depth=MAX_DEPTH, n_estimators=N_ESTIMATORS,
-                                      learning_rate=LEARNING_RATE)
+                                       learning_rate=LEARNING_RATE)
     model.fit(X_train, y_train)
     print("finished fittin the model")
     results.update({
+        'ntrees': model.get_n_trees(),
+        'nleaves': model.get_n_leaves(),
+        'rmse': model.compute_rmse(X_test, y_test),
         'gain': model.compute_fi_gain().to_dict(),
         'permutation_train': model.compute_fi_permutation(X_train, y_train).to_dict(),
         'permutation_test': model.compute_fi_permutation(X_test, y_test).to_dict(),
         'shap_train': model.compute_fi_shap(X_train, y_train).to_dict(),
-        'shap_test': model.compute_fi_shap(X_test, y_test).to_dict()})
+        'shap_test': model.compute_fi_shap(X_test, y_test).to_dict()
+    })
     DataFrame(Series(results)).T.to_csv(exp_results_path)
 
 
 if __name__ == '__main__':
-    DEBUG =True
+    DEBUG = True
     MODELS = {
         'lgbm': ['vanilla'],
-        # 'xgboost': ['one_hot', 'mean_imputing'],
-        # 'catboost': ['vanilla'],
-        # 'sklearn': ['one_hot', 'mean_imputing'],
-        # 'ours': ['Kfold', 'CartVanilla']
-        }
+        'xgboost': ['one_hot', 'mean_imputing'],
+        'catboost': ['vanilla'],
+        'sklearn': ['one_hot', 'mean_imputing'],
+        'ours': ['Kfold', 'CartVanilla']
+    }
 
     make_dirs([RESULTS_DIR])
     for model_name, model_variants in MODELS.items():

fd8ed6efd60b6f46902b291ea1e5df782f921235
Afek <afekilayadler@gmail.com>
2020-05-13 19:36:59 +0300

    updated experiments

--- experiments/bike_rentals_regression/bike_rentals_regression.py
+++ experiments/bike_rentals_regression/bike_rentals_regression.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from pandas import Series, DataFrame, read_csv, to_datetime
 from sklearn.model_selection import train_test_split
 
+from algorithms.Tree.fast_tree.bining import BinMapper
+from algorithms.Tree.utils import get_num_cols
 from experiments.default_config import RESULTS_DIR, VAL_RATIO, MAX_DEPTH, N_ESTIMATORS, LEARNING_RATE, GBM_REGRESSORS
 from experiments.utils import make_dirs, transform_categorical_features
 
@@ -26,7 +28,7 @@ def get_x_y():
     return X, y
 
 
-def worker(model_name, variant):
+def worker(model_name, variant, fast):
     exp_name = F"{model_name}_{variant}.csv"
     dir = RESULTS_DIR / model_name
     exp_results_path = dir / exp_name
@@ -35,10 +37,16 @@ def worker(model_name, variant):
     X, y = get_x_y()
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size=VAL_RATIO, random_state=42)
+    # if fast and model_name == 'ours':
+    num_cols = get_num_cols(X.dtypes)
+    bin_mapper = BinMapper(max_bins=256, random_state=42)
+    X_train.loc[:,num_cols]= bin_mapper.fit_transform(X_train.loc[:,num_cols].values)
+    X_test.loc[:,num_cols] = bin_mapper.transform(X_test.loc[:,num_cols].values)
+
     results = {'model': F"{model_name}_{variant}"}
     X_train, X_test = transform_categorical_features(X_train, X_test, y_train, variant)
     model = GBM_REGRESSORS[model_name](variant, X.dtypes, max_depth=MAX_DEPTH, n_estimators=N_ESTIMATORS,
-                                       learning_rate=LEARNING_RATE)
+                                       learning_rate=LEARNING_RATE, fast = fast)
     model.fit(X_train, y_train)
     print("finished fittin the model")
     results.update({
@@ -55,7 +63,8 @@ def worker(model_name, variant):
 
 
 if __name__ == '__main__':
-    DEBUG = True
+    FAST = True
+    DEBUG = False
     MODELS = {
         'lgbm': ['vanilla'],
         'xgboost': ['one_hot', 'mean_imputing'],
@@ -71,9 +80,9 @@ if __name__ == '__main__':
         make_dirs([RESULTS_DIR / model_name])
         for variant in model_variants:
             if DEBUG:
-                worker(model_name, variant)
+                worker(model_name, variant, FAST)
             else:
-                args.append((model_name, variant))
+                args.append((model_name, variant, FAST))
         if not DEBUG:
             with multiprocessing.Pool() as process_pool:
                 process_pool.starmap(worker, args)
--- experiments/gbm_boston_house_pricing/gbm_boston_house_pricing.py
+++ experiments/gbm_boston_house_pricing/gbm_boston_house_pricing.py
@@ -1,10 +1,12 @@
 import multiprocessing
 
-from numpy import arange
+from numpy import arange, random
 from pandas import Series, DataFrame
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 
+from algorithms.Tree.fast_tree.bining import BinMapper
+from algorithms.Tree.utils import get_num_cols
 from experiments.default_config import RESULTS_DIR, VAL_RATIO, MAX_DEPTH, N_ESTIMATORS, LEARNING_RATE, GBM_REGRESSORS
 from experiments.utils import make_dirs, transform_categorical_features
 
@@ -13,10 +15,10 @@ def get_x_y():
     data = load_boston()
     X = DataFrame(data['data'], columns=data['feature_names'])
     y = Series(data['target'])
-    """
-    add cat index
-    """
-    # X['index'] = arange(X.shape[0])
+    # """
+    # add cat index
+    # """
+    # X['index'] = random.randint(0,500,X.shape[0])
     # X['index'] = X['index'].astype('category')
     return X, y
 
@@ -33,6 +35,12 @@ def worker(model_name, variant, fast):
     results = {'model': F"{model_name}_{variant}"}
 
     X_train, X_test = transform_categorical_features(X_train, X_test, y_train, variant)
+    if fast and model_name == 'ours':
+        num_cols = get_num_cols(X.dtypes)
+        bin_mapper = BinMapper(max_bins=256, random_state=42)
+        X_train.loc[:,num_cols]= bin_mapper.fit_transform(X_train.loc[:,num_cols].values)
+        X_test.loc[:,num_cols] = bin_mapper.transform(X_test.loc[:,num_cols].values)
+
     model = GBM_REGRESSORS[model_name](variant, X.dtypes, max_depth=MAX_DEPTH, n_estimators=N_ESTIMATORS,
                                        learning_rate=LEARNING_RATE, fast=fast)
     model.fit(X_train, y_train)
@@ -53,11 +61,11 @@ if __name__ == '__main__':
     FAST = True
     DEBUG = True
     MODELS = {
-        # 'lgbm': ['vanilla'],
-        # 'xgboost': ['mean_imputing'],
-        # 'catboost': ['vanilla'],
-        # 'sklearn': ['mean_imputing'],
-        'ours': ['Kfold', 'CartVanilla']}
+        'lgbm': ['vanilla'],
+        'xgboost': ['mean_imputing'],
+        'catboost': ['vanilla'],
+        'sklearn': ['mean_imputing'],
+        'ours': [ 'CartVanilla', 'Kfold',]}
 
     make_dirs([RESULTS_DIR])
     for model_name, model_variants in MODELS.items():

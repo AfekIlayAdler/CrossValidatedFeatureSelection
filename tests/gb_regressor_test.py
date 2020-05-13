963a9970ee67f1f82427cad6c70a59a81fa97c6d
Afek <afekilayadler@gmail.com>
2020-05-13 19:38:36 +0300

    seperated data from test

--- tests/gb_regressor_test.py
+++ tests/gb_regressor_test.py
@@ -1,23 +1,40 @@
-import numpy as np
 import time
-from algorithms.Tree import TreeVisualizer
-from algorithms import CartGradientBoostingRegressorKfold, CartGradientBoostingRegressor
-from tests.test_dataset_creator import create_x_y
 
-np.seterr(all='raise')
-EXP = 'simulation'  # 'simulation'
-KFOLD = True
-MAX_DEPTH = 3
-reg = CartGradientBoostingRegressorKfold(max_depth=3) if KFOLD else CartGradientBoostingRegressor(max_depth=3)
-np.random.seed(3)
-X, y = create_x_y()
-start = time.time()
-reg.fit(X, y)
-end = time.time()
-print(end - start)
-tree_vis = TreeVisualizer()
-tree_vis.plot(reg.trees[0].root)
-# tree_vis.plot(reg.trees[1].root)
-# tree_vis.plot(reg.trees[2].root)
-print(reg.n_trees)
-print(reg.compute_feature_importance())
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from algorithms import CartGradientBoostingRegressorKfold, CartGradientBoostingRegressor, \
+    FastCartGradientBoostingRegressorKfold, FastCartGradientBoostingRegressor
+from algorithms.Tree import TreeVisualizer
+from tests.get_xy import get_x_y_boston
+
+if __name__ == "__main__":
+    from sklearn.metrics import mean_squared_error
+
+    np.seterr(all='raise')
+    KFOLD = True
+    FAST = True
+    BOSTON = True
+    BIKE_RENTALS = False
+    MAX_DEPTH = 3
+    np.random.seed(3)
+    if FAST:
+        model = FastCartGradientBoostingRegressorKfold if KFOLD else FastCartGradientBoostingRegressor
+    else:
+        model = CartGradientBoostingRegressorKfold if KFOLD else CartGradientBoostingRegressor
+    reg = model(max_depth=3)
+
+    start = time.time()
+    X, y = get_x_y_boston()
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.1, random_state=42)
+    reg.fit(X_train, y_train)
+    end = time.time()
+    print(end - start)
+    print(f"mse is {mean_squared_error(y_test, reg.predict(X_test))}")
+    tree_vis = TreeVisualizer()
+    tree_vis.plot(reg.trees[0])
+    # tree_vis.plot(reg.trees[1].root)
+    # tree_vis.plot(reg.trees[2].root)
+    print(reg.n_trees)
+    print(reg.compute_feature_importance())

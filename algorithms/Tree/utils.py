0e404a6f5a36a8aecca6d5b642f8b2f2e8511c78
Afek <afekilayadler@gmail.com>
2020-05-13 19:37:35 +0300

    added get_num_cols and get_cat_cols

--- algorithms/Tree/utils.py
+++ algorithms/Tree/utils.py
@@ -10,14 +10,21 @@ def get_cols_dtypes(df: DataFrame) -> Dict:
     return {i: v.name for i, v in df.dtypes.to_dict().items()}
 
 
+def get_cat_cols(dtypes: Series):
+    return dtypes[dtypes == 'category'].index.tolist()
+
+
+def get_num_cols(dtypes : Series):
+    return dtypes[dtypes != 'category'].index.tolist()
+
+
 def get_cat_num_cols(dtypes):
-    cat = [col for col, dtype in dtypes.items() if dtype == 'category']
-    num = [col for col, dtype in dtypes.items() if dtype != 'category']
-    return cat, num
+    return get_cat_cols(dtypes), get_num_cols(dtypes)
 
 
-def get_n_unique_values_per_cat(X):
-    return array([len(unique(X[:, col])) for col in range(X.shape[1])])
+def get_max_value_per_cat(X):
+    return array([X[:, col].max() for col in range(X.shape[1])])
+
 
 def get_col_type(col_type):
     if col_type in PANDAS_CATEGORICAL_COLS:
@@ -30,7 +37,7 @@ def regression_impurity(y: Series):
 
 
 def classification_impurity(y: Series):
-    p = sum(y)/y.size
+    p = sum(y) / y.size
     n = y.size
     return n * p * (1 - p)
 

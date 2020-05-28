da019020eb8549e8f24d91e805bfd5b3e57916b3
Afek <afekilayadler@gmail.com>
2020-05-28 11:58:06 -0600

    added gbm classifiers

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,10 +1,12 @@
 from .Tree import CartClassificationTreeKFold, CartRegressionTreeKFold, CartClassificationTree, CartRegressionTree
 from .Tree import TreeVisualizer
 from .Tree import node_based_feature_importance
-from .random_forest import CartKfoldRandomForestClassifier, CartRandomForestRegressor, CartRandomForestClassifier, CartKfoldRandomForestRegressor
+from .random_forest import CartKfoldRandomForestClassifier, CartRandomForestRegressor, CartRandomForestClassifier,\
+    CartKfoldRandomForestRegressor
 from .gradient_boosting_regressor import CartGradientBoostingRegressor, CartGradientBoostingRegressorKfold, \
     FastCartGradientBoostingRegressor, FastCartGradientBoostingRegressorKfold
-from .gradient_boosting_classifier import CartGradientBoostingClassifier
+from .gradient_boosting_classifier import CartGradientBoostingClassifier, FastCartGradientBoostingClassifier, \
+    CartGradientBoostingClassifierKfold, FastCartGradientBoostingClassifierKfold
 from .config import N_ESTIMATORS, LEARNING_RATE
 
 

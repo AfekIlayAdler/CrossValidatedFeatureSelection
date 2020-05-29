f5b3859b9a4fc4ad881bd4f15a2cf4903237daae
Afek <afekilayadler@gmail.com>
2020-05-29 14:50:22 -0600

    added experiment

--- experiments/criteo_ctr/criteo_ctr_classification.py
+++ experiments/criteo_ctr/criteo_ctr_classification.py
@@ -1,17 +1,24 @@
-from pandas import Series, DataFrame
+from pathlib import Path
+
+from pandas import Series, DataFrame, read_csv
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
 from algorithms.Tree.fast_tree.bining import BinMapper
 from algorithms.Tree.utils import get_num_cols
 from experiments.default_config import RESULTS_DIR, VAL_RATIO, MAX_DEPTH, N_ESTIMATORS, LEARNING_RATE, GBM_CLASSIFIERS
+from experiments.preprocess_pipelines import get_preprocessing_pipeline
 from experiments.utils import make_dirs, transform_categorical_features
 
 
 def get_x_y():
-    data = load_breast_cancer()
-    X = DataFrame(data['data'], columns=data['feature_names'])
-    y = Series(data['target'])
+    project_root = Path(__file__).parent.parent.parent
+    y_col_name = 'click'
+    train = read_csv(project_root / 'datasets/criteo_ctr_prediction/train_10000.csv')
+    y = train[y_col_name]
+    X = train.drop(columns=[y_col_name])
+    for col in X.columns:
+        X[col] = X[col].astype('category')
     return X, y
 
 
@@ -19,19 +26,18 @@ def worker(model_name, variant):
     exp_name = F"{model_name}_{variant}.csv"
     dir = RESULTS_DIR / model_name
     exp_results_path = dir / exp_name
-    # if exp_results_path.exists():
-    #     return
+    if exp_results_path.exists():
+        return
     X, y = get_x_y()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=VAL_RATIO)
-    if model_name.startswith('ours'):
-        num_cols = get_num_cols(X.dtypes)
-        bin_mapper = BinMapper(max_bins=256, random_state=42)
-        X_train.loc[:, num_cols] = bin_mapper.fit_transform(X_train.loc[:, num_cols].values)
-        X_test.loc[:, num_cols] = bin_mapper.transform(X_test.loc[:, num_cols].values)
-
+    pipeline = get_preprocessing_pipeline(0.5, cols_to_remove)
+    pipeline.fit(X_train)
+    X_train = pipeline.transform(X_train)
+    X_test = pipeline.transform(X_test)
     results = {'model': F"{model_name}_{variant}"}
+    original_dtypes = X_train.dtypes
     X_train, X_test = transform_categorical_features(X_train, X_test, y_train, variant)
-    model = GBM_CLASSIFIERS[model_name](variant, X.dtypes, max_depth=MAX_DEPTH, n_estimators=N_ESTIMATORS,
+    model = GBM_CLASSIFIERS[model_name](variant, original_dtypes, max_depth=MAX_DEPTH, n_estimators=N_ESTIMATORS,
                                         learning_rate=LEARNING_RATE, subsample=1.)
     model.fit(X_train, y_train)
     results.update({
@@ -44,20 +50,18 @@ def worker(model_name, variant):
         'shap_train': model.compute_fi_shap(X_train, y_train).to_dict(),
         'shap_test': model.compute_fi_shap(X_test, y_test).to_dict()
     })
-    print("************")
-    print(model.compute_f1(X_test, y_test))
-    print("************")
     DataFrame(Series(results)).T.to_csv(exp_results_path)
 
 
 if __name__ == '__main__':
+    cols_to_remove = ['hour', 'id', 'Unnamed: 0']
     MODELS = {
         'lgbm': ['vanilla'],
-        # 'xgboost': ['mean_imputing'],
-        # 'catboost': ['vanilla'],
-        # 'sklearn': ['mean_imputing'],
-        # 'ours_vanilla': ['_'],
-        # 'ours_kfold': ['_']
+        'xgboost': ['mean_imputing'],
+        'catboost': ['vanilla'],
+        'sklearn': ['mean_imputing'],
+        'ours_vanilla': ['_'],
+        'ours_kfold': ['_']
     }
 
     make_dirs([RESULTS_DIR])

5f6c11de87e65a8c549eb5c34c0a99413bdb0d63
Afek <afekilayadler@gmail.com>
2020-05-28 18:15:12 -0600

    removed fast option and change model names

--- experiments/gbm_boston_house_pricing/gbm_boston_house_pricing.py
+++ experiments/gbm_boston_house_pricing/gbm_boston_house_pricing.py
@@ -1,14 +1,10 @@
-from numpy import random
-from numpy.random import seed
 from pandas import Series, DataFrame
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
 
 from algorithms.Tree.fast_tree.bining import BinMapper
 from algorithms.Tree.utils import get_num_cols
-from experiments.default_config import RESULTS_DIR, VAL_RATIO, MAX_DEPTH, N_ESTIMATORS, LEARNING_RATE, GBM_REGRESSORS, \
-    SUBSAMPLE
+from experiments.default_config import RESULTS_DIR, VAL_RATIO, MAX_DEPTH, N_ESTIMATORS, LEARNING_RATE, GBM_REGRESSORS
 from experiments.utils import make_dirs, transform_categorical_features
 
 
@@ -24,16 +20,15 @@ def get_x_y():
     return X, y
 
 
-def worker(model_name, variant, fast):
+def worker(model_name, variant):
     exp_name = F"{model_name}_{variant}.csv"
     dir = RESULTS_DIR / model_name
     exp_results_path = dir / exp_name
-    # seed(exp_number)
     if exp_results_path.exists():
         return
     X, y = get_x_y()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=VAL_RATIO)
-    if fast and model_name == 'ours':
+    if model_name.startswith('ours'):
         num_cols = get_num_cols(X.dtypes)
         bin_mapper = BinMapper(max_bins=256, random_state=42)
         X_train.loc[:, num_cols] = bin_mapper.fit_transform(X_train.loc[:, num_cols].values)
@@ -42,7 +37,7 @@ def worker(model_name, variant, fast):
     results = {'model': F"{model_name}_{variant}"}
     X_train, X_test = transform_categorical_features(X_train, X_test, y_train, variant)
     model = GBM_REGRESSORS[model_name](variant, X.dtypes, max_depth=MAX_DEPTH, n_estimators=N_ESTIMATORS,
-                                       learning_rate=LEARNING_RATE, subsample=1., fast=fast)
+                                       learning_rate=LEARNING_RATE, subsample=1.)
     model.fit(X_train, y_train)
     results.update({
         'ntrees': model.get_n_trees(),
@@ -58,24 +53,19 @@ def worker(model_name, variant, fast):
 
 
 if __name__ == '__main__':
-    N_EXPERIMENTS = 30
-    FAST = True
-    DEBUG = True
     MODELS = {
         'lgbm': ['vanilla'],
         'xgboost': ['mean_imputing'],
         'catboost': ['vanilla'],
         'sklearn': ['mean_imputing'],
-        'ours': ['CartVanilla', 'Kfold',]}
+        'ours_vanilla': ['_'],
+        'ours_kfold': ['_']
+    }
 
     make_dirs([RESULTS_DIR])
     for model_name, model_variants in MODELS.items():
         print(f'Working on experiment : {model_name}')
         make_dirs([RESULTS_DIR / model_name])
-        with tqdm(total=N_EXPERIMENTS * len(model_variants)) as pbar:
-            for variant in model_variants:
-                # for exp in range(N_EXPERIMENTS):
-                worker(model_name, variant, FAST)
-                # print(f"Finished exp # {exp}")
-                pbar.update(1)
+        for variant in model_variants:
+            worker(model_name, variant)
     print("run took {end - time}")

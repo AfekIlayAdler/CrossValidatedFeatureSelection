198976ad1829eb3c1a4c92646c39236b815567fa
Afek <afekilayadler@gmail.com>
2020-05-31 11:55:27 -0600

    added regression error and classification error

--- experiments/moodel_wrappers/wrapper_utils.py
+++ experiments/moodel_wrappers/wrapper_utils.py
@@ -1,5 +1,6 @@
 import shap
 from pandas import DataFrame
+from sklearn.metrics import mean_squared_error, f1_score
 
 
 def normalize_series(s):
@@ -12,5 +13,16 @@ def get_shap_values(model, x, columns):
     shap_values = shap.TreeExplainer(model, feature_perturbation="tree_path_dependent").shap_values(x)
     if type(shap_values) == list:
         shap_values = shap_values[0]
-    abs_shap_values = DataFrame(shap_values,columns=columns).apply(abs)
+    abs_shap_values = DataFrame(shap_values, columns=columns).apply(abs)
     return abs_shap_values.mean() / abs_shap_values.mean().sum()
+
+
+def regression_error(y_true, y_pred, inverse=False):
+    return mean_squared_error(y_true, y_pred)
+
+
+def classification_error(y_true, y_pred, inverse=True):
+    f1 = f1_score(y_true, (y_pred > 0.5) * 1)
+    if inverse:
+        return 1 - f1
+    return f1

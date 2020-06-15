ab96da1d58c2e5ee95c03fccbe0e51da0c67c654
Afek <afekilayadler@gmail.com>
2020-06-15 10:27:23 -0600

    added amazon test

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -1,17 +1,19 @@
 import time
+from pathlib import Path
 
 from numpy import random
+from pandas import read_csv
 
 from algorithms import CartRegressionTreeKFold, CartRegressionTree, TreeVisualizer, node_based_feature_importance
 from algorithms.Tree import FastCartRegressionTree, FastCartRegressionTreeKFold
 from algorithms.Tree.utils import get_cat_num_cols
-from tests.get_xy import get_x_y_boston, get_x_y_bike_rentals
-from tests.test_dataset_creator import create_x_y
+from tests.get_xy import get_x_y_boston, get_x_y_bike_rentals, get_x_y_amazon
+from tests.get_xy import create_x_y
 
 if __name__ == "__main__":
-    KFOLD = False
-    FAST = True
-    DATA = 'BIKE_RANTAL' # BOSTON
+    KFOLD = True
+    FAST = False
+    DATA = 'AMAZON'  # 'BIKE_RANTAL'  BOSTON
     MAX_DEPTH = 3
     if FAST:
         model = FastCartRegressionTreeKFold if KFOLD else FastCartRegressionTree
@@ -26,9 +28,12 @@ if __name__ == "__main__":
     elif DATA == 'BIKE_RANTAL':
         X, y = get_x_y_bike_rentals()
         cat_cols, num_cols = get_cat_num_cols(X.dtypes)
-        X = X#[cat_cols]
+        X = X  # [cat_cols]
+    elif DATA == 'AMAZON':
+        X, y = get_x_y_amazon()
     else:
         X, y = create_x_y()
+
     start = time.time()
     tree.fit(X, y)
     end = time.time()

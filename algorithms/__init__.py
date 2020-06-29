57ab39004302c790b06d0c72a9e7643c9879855d
Afek <afekilayadler@gmail.com>
2020-06-29 17:27:43 +0300

    updared random forest

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,12 +1,20 @@
 from .Tree import CartClassificationTreeKFold, CartRegressionTreeKFold, CartClassificationTree, CartRegressionTree
 from .Tree import TreeVisualizer
 from .Tree import node_based_feature_importance
-from .random_forest import CartKfoldRandomForestClassifier, CartRandomForestRegressor, CartRandomForestClassifier,\
-    CartKfoldRandomForestRegressor
+
 from .gradient_boosting_regressor import CartGradientBoostingRegressor, CartGradientBoostingRegressorKfold, \
     FastCartGradientBoostingRegressor, FastCartGradientBoostingRegressorKfold
 from .gradient_boosting_classifier import CartGradientBoostingClassifier, FastCartGradientBoostingClassifier, \
     CartGradientBoostingClassifierKfold, FastCartGradientBoostingClassifierKfold
+
+
+from .random_forest_classifier import CartRandomForestClassifier, CartKfoldRandomForestClassifier, \
+    FastCartRandomForestClassifier, FastCartKfoldRandomForestClassifier
+
+from .random_forest_regressor import CartRandomForestRegressor, CartKfoldRandomForestRegressor, \
+    FastCartRandomForestRegressor, FastCartKfoldRandomForestRegressor
+
+
 from .config import N_ESTIMATORS, LEARNING_RATE
 
 

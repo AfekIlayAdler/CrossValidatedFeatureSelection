9f0ad02611fa787e0fffa7820bb8fb2f8d694578
Afek <afekilayadler@gmail.com>
2020-06-29 11:49:30 +0300

    now support new run_experiment.py

--- experiments/gbm_classification/amazon/amazon.py
+++ experiments/gbm_classification/amazon/amazon.py
@@ -13,7 +13,7 @@ def get_x_y():
     y_col_name = 'ACTION'
     train = read_csv(project_root / 'datasets/amazon_from_catboost_paper/train.csv')
     y = train[y_col_name]
-    X = train.drop(columns=[y_col_name])
+    X = train.drop(columns=[y_col_name, 'ROLE_FAMILY_DESC'])
     for col in X.columns:
         X[col] = X[col].astype('category')
     return X, y
@@ -21,13 +21,15 @@ def get_x_y():
 
 if __name__ == '__main__':
     config = Config(
+        kfold_flag = True,
         compute_permutation=False,
         save_results=True,
         one_hot=False,  # takes to much time
         contains_num_features=False,
         seed=7,
+        kfolds = 30,
         predictors=GBM_CLASSIFIERS,
         columns_to_remove=[],
         get_x_y=get_x_y,
         preprocessing_pipeline=get_preprocessing_pipeline_only_cat)
-    run_experiments(config, 30)
+    run_experiments(config)

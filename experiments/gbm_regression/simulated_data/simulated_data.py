4331610df417edef647e0cfba1291dcf6a6e87f8
Afek <afekilayadler@gmail.com>
2020-07-06 10:34:31 +0300

    modified experiment

--- experiments/gbm_regression/simulated_data/simulated_data.py
+++ experiments/gbm_regression/simulated_data/simulated_data.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-from numpy import random
+from numpy import random, zeros
 from pandas import read_csv, to_datetime, DataFrame, Series
 
 from experiments.config_object import Config
 from experiments.default_config import GBM_REGRESSORS
-from experiments.preprocess_pipelines import get_preprocessing_pipeline
+from experiments.preprocess_pipelines import get_preprocessing_pipeline, get_preprocessing_pipeline_only_cat
 from experiments.run_experiment import run_experiments
 
 """
@@ -16,24 +16,32 @@ dataset from kaggle. contains both categorical and numerical features. ~11k samp
 def get_x_y():
     X = DataFrame()
     nrows = 10**3
+    y = Series(zeros(nrows))
     for i in range(1,7):
-        X[i] = random.randint(0, 2**1, nrows)
-    X['continues'] = random.random(nrows)
-    y = Series(random.random(nrows))
+        X[i] = random.randint(0, 2**i, nrows)
+        X[i] = X[i].astype('category')
+        y += X[i].isin(list(range(2**(i-1))))*1
+        # X[i] = random.random(nrows)
+        # y += (X[i] <= 0.5) * 0.5
+
+    X[8] = random.random(nrows)
+    y +=  random.random(nrows) + (X[8] <= 0) * 0.5
+    # y = Series(random.random(nrows))
     return X, y
 
 
 if __name__ == '__main__':
     config = Config(
+        drop_one_feature_flag = False,
         kfold_flag=False,
-        compute_permutation=True,
+        compute_permutation=False,
         save_results=True,
         one_hot=False,  # when we add index then it takes to much time..
-        contains_num_features=True,
-        seed=7,
+        contains_num_features=True, # True
+        seed=10,
         kfolds=30,
         predictors=GBM_REGRESSORS,
         columns_to_remove=[],
         get_x_y=get_x_y,
-        preprocessing_pipeline=get_preprocessing_pipeline)
+        preprocessing_pipeline=get_preprocessing_pipeline) #  get_preprocessing_pipeline
     run_experiments(config)

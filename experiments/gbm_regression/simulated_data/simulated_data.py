e55e7204f576767e32f32b61491513dd317ea2a5
Afek <afekilayadler@gmail.com>
2020-07-13 18:10:09 +0300

    updated default experiment

--- experiments/gbm_regression/simulated_data/simulated_data.py
+++ experiments/gbm_regression/simulated_data/simulated_data.py
@@ -1,47 +1,49 @@
-from pathlib import Path
-
 from numpy import random, zeros
-from pandas import read_csv, to_datetime, DataFrame, Series
+from pandas import DataFrame, Series
 
 from experiments.config_object import Config
-from experiments.default_config import GBM_REGRESSORS
-from experiments.preprocess_pipelines import get_preprocessing_pipeline, get_preprocessing_pipeline_only_cat
+from experiments.default_config import GBM_REGRESSORS, GBM_CLASSIFIERS, SEED, KFOLDS, N_EXPERIMENTS
 from experiments.experiment_configurator import experiment_configurator
-
-"""
-dataset from kaggle. contains both categorical and numerical features. ~11k samples
-"""
+from experiments.preprocess_pipelines import get_preprocessing_pipeline_only_cat, get_preprocessing_pipeline
 
 
 def get_x_y():
     X = DataFrame()
-    nrows = 10**3
+    nrows = 10 ** 3
     y = Series(zeros(nrows))
-    for i in range(1,7):
-        X[i] = random.randint(0, 2**i, nrows)
+    for i in range(1, 7):
+        X[i] = random.randint(0, 2 ** i, nrows)
         X[i] = X[i].astype('category')
-        y += X[i].isin(list(range(2**(i-1))))*1
-        # X[i] = random.random(nrows)
-        # y += (X[i] <= 0.5) * 0.5
+        y += X[i].isin(list(range(2 ** (i - 1)))) * 1
 
-    X[8] = random.random(nrows)
-    y +=  random.random(nrows) + (X[8] <= 0) * 0.5
-    # y = Series(random.random(nrows))
+    y += random.random(nrows)
+    y = Series(random.random(nrows))
     return X, y
 
 
 if __name__ == '__main__':
+    MULTIPLE_EXPERIMENS = True
+    KFOLD = False
+    ONE_HOT = False
+    COMPUTE_PERMUTATION = True
+    CONTAINS_NUM_FEATURES = False
+
+    REGRESSION = True
+    pp = get_preprocessing_pipeline if CONTAINS_NUM_FEATURES else get_preprocessing_pipeline_only_cat
+    predictors = GBM_REGRESSORS if REGRESSION else GBM_CLASSIFIERS
+
     config = Config(
-        drop_one_feature_flag = False,
-        kfold_flag=False,
-        compute_permutation=False,
+        multiple_experimens=MULTIPLE_EXPERIMENS,
+        n_experiments=N_EXPERIMENTS,
+        kfold_flag=KFOLD,
+        compute_permutation=COMPUTE_PERMUTATION,
         save_results=True,
-        one_hot=False,  # when we add index then it takes to much time..
-        contains_num_features=True, # True
-        seed=10,
-        kfolds=30,
-        predictors=GBM_REGRESSORS,
+        one_hot=ONE_HOT,
+        contains_num_features=CONTAINS_NUM_FEATURES,
+        seed=SEED,
+        kfolds=KFOLDS,
+        predictors=predictors,
         columns_to_remove=[],
         get_x_y=get_x_y,
-        preprocessing_pipeline=get_preprocessing_pipeline) #  get_preprocessing_pipeline
+        preprocessing_pipeline=pp)
     experiment_configurator(config)

9cec6978fb56b0ced62efddf1e52b999247d4980
Afek <afekilayadler@gmail.com>
2020-07-13 18:12:45 +0300

    removed loo

--- experiments/experiment_configurator.py
+++ experiments/experiment_configurator.py
@@ -21,6 +21,8 @@ def experiment_configurator(config):
             seed(config.seed)
             configurations = get_configurations(config, model_name, variant, exp_dir)
             for configuration in configurations:
+                if config.exp_results_path.exists():
+                    continue
                 run_experiment(configuration)
 
     end = time()
@@ -28,10 +30,21 @@ def experiment_configurator(config):
 
 
 def get_configurations(config, model_name, variant, exp_dir):
-    if config.kfold_flag:
+    # if config.multiple_experimens:
+    #     return multiple_experiments_configuration(config, model_name, variant, exp_dir)
+    # elif config.kfold_flag and config.drop_one_feature_flag:
+    #     return get_kfold_drop_one_feature_configuration(config, model_name, variant, exp_dir)
+    # elif config.kfold_flag:
+    #     return get_kfold_configuration(config, model_name, variant, exp_dir)
+    # elif config.drop_one_feature_flag:
+    #     return get_drop_one_feature_configuration(config, model_name, variant, exp_dir)
+    # else:
+    #     return get_regular_configuration(config, model_name, variant, exp_dir)
+
+    if config.multiple_experimens:
+        return multiple_experiments_configuration(config, model_name, variant, exp_dir)
+    elif config.kfold_flag:
         return get_kfold_configuration(config, model_name, variant, exp_dir)
-    elif config.drop_one_feature_flag:
-        return get_drop_one_feature_configuration(config, model_name, variant, exp_dir)
     else:
         return get_regular_configuration(config, model_name, variant, exp_dir)
 
@@ -44,28 +57,12 @@ def get_variants(is_gbm: bool, one_hot: bool):
     return dict(sklearn=variants, ours_vanilla=['_'], ours_kfold=['_'])
 
 
-def get_regular_configuration(config, model_name, variant, exp_dir):
-    X, y = config.get_x_y()
-    config._set_attributes(
-        exp_results_path=exp_dir / F"{model_name}_{variant}.csv",
-        data=train_test_split(X, y, test_size=VAL_RATIO))
-    yield config
-
-
-def get_drop_one_feature_configuration(config, model_name, variant, exp_dir):
-    X, y = config.get_x_y()
-    config._set_attributes(
-        exp_results_path=exp_dir / F"{model_name}_{variant}.csv",
-        data=train_test_split(X, y, test_size=VAL_RATIO),
-        compute_permutation=True)
-    yield config
-    for col in X.columns.tolist():
-        cols_to_remove = config.columns_to_remove + col
+def multiple_experiments_configuration(config, model_name, variant, exp_dir):
+    for i in range(config.n_experiments):
+        X, y = config.get_x_y()
         config._set_attributes(
-            exp_results_path=exp_dir / F"{model_name}_{variant}_{col}.csv",
-            data=train_test_split(X, y, test_size=VAL_RATIO),
-            compute_permutation=False,
-            columns_to_remove=cols_to_remove)
+            exp_results_path=exp_dir / F"{model_name}_{variant}_{i}.yaml",
+            data=train_test_split(X, y, test_size=VAL_RATIO))
         yield config
 
 
@@ -74,7 +71,54 @@ def get_kfold_configuration(config, model_name, variant, exp_dir):
     kf = KFold(n_splits=config.kfolds, shuffle=True)
     for i, (train_index, test_index) in enumerate(kf.split(X)):
         config._set_attributes(
-            exp_results_path=exp_dir / F"{model_name}_{variant}_{i}.csv",
+            exp_results_path=exp_dir / F"{model_name}_{variant}_{i}.yaml",
             data=(X.iloc[train_index], X.iloc[test_index], y.iloc[train_index], y.iloc[test_index]),
             compute_permutation=False)
         yield config
+
+
+def get_regular_configuration(config, model_name, variant, exp_dir):
+    X, y = config.get_x_y()
+    config._set_attributes(
+        exp_results_path=exp_dir / F"{model_name}_{variant}.yaml",
+        data=train_test_split(X, y, test_size=VAL_RATIO))
+    yield config
+
+# def get_drop_one_feature_configuration(config, model_name, variant, exp_dir):
+#     X, y = config.get_x_y()
+#     config._set_attributes(
+#         exp_results_path=exp_dir / F"{model_name}_{variant}.yaml",
+#         data=train_test_split(X, y, test_size=VAL_RATIO),
+#         compute_permutation=True)
+#     yield config
+#     columns_to_remove = config.columns_to_remove
+#     for col in X.columns.tolist():
+#         config._set_attributes(
+#             exp_results_path=exp_dir / F"{model_name}_{variant}_{col}.yaml",
+#             data=train_test_split(X, y, test_size=VAL_RATIO),
+#             compute_permutation=False,
+#             columns_to_remove= columns_to_remove + [col])
+#         yield config
+
+
+# def get_kfold_drop_one_feature_configuration(config, model_name, variant, exp_dir):
+#     X, y = config.get_x_y()
+#     kf = KFold(n_splits=config.kfolds, shuffle=True)
+#     for i, (train_index, test_index) in enumerate(kf.split(X)):
+#         data = (X.iloc[train_index], X.iloc[test_index], y.iloc[train_index], y.iloc[test_index])
+#         config._set_attributes(
+#             exp_results_path=exp_dir / F"{model_name}_{variant}_{i}.yaml",
+#             data=data,
+#             compute_permutation=True)
+#         yield config
+#
+#     columns_to_remove = config.columns_to_remove
+#     for i, (train_index, test_index) in enumerate(kf.split(X)):
+#         data = (X.iloc[train_index], X.iloc[test_index], y.iloc[train_index], y.iloc[test_index])
+#         for col in X.columns.tolist():
+#             config._set_attributes(
+#                 exp_results_path=exp_dir / F"{model_name}_{variant}_{col}_{i}.yaml",
+#                 data=data,
+#                 compute_permutation=False,
+#                 columns_to_remove= columns_to_remove + [col])
+#             yield config

d30fcc74c9ea30a20665c61c3448a6cd6ea67d9c
Afek <afekilayadler@gmail.com>
2020-10-23 11:47:13 +0300

    fixd experiment function

--- experiments/gbm_regression/simulated_data/simulations_from_strobl_paper/simulated_data.py
+++ experiments/gbm_regression/simulated_data/simulations_from_strobl_paper/simulated_data.py
@@ -1,3 +1,4 @@
+from functools import partial
 from pathlib import Path
 
 from numpy import random, zeros
@@ -10,59 +11,56 @@ from experiments.experiment_configurator import experiment_configurator
 from experiments.preprocess_pipelines import get_preprocessing_pipeline_only_cat, get_preprocessing_pipeline
 
 
-def get_x_y():
-    y_is_noise = False
-    num_feature = False
-    num_feature_connected = False
-    connected_range = {5}
+def get_x_y(alpha=0.2, null=True):
+    if null:
+        alpha = 0
 
+    nrows = 6000
     X = DataFrame()
-    nrows = 10 ** 4
-    y = Series(zeros(nrows))
-    for i in range(5, 10):
-        X[i] = random.randint(0, 2 ** i, nrows)
-        X[i] = X[i].astype('category')
-        if i in connected_range:
-            y += X[i].isin(list(range(2 ** (i - 2)))) * 1
-
-    X.columns = [str(i) for i in X.columns]
-    if num_feature:
-        X['numeric'] = random.random(nrows) * 1
-        if num_feature_connected:
-            y += (X['numeric'] <= 0) * 1
-
-    y += random.random(nrows) * 3
-    if y_is_noise:
-        y = Series(random.random(nrows))
+    X['X0'] = random.random(nrows)
+    X['X1'] = random.randint(0, 10, nrows)
+    X['X2'] = random.randint(0, 20, nrows)
+    X['X3'] = random.randint(0, 50, nrows)
+    X['X4'] = random.randint(0, 100, nrows)
+    for col in X.columns[1:]:
+        X[col] = X[col].astype('category')
+    y = []
+    for i in range(nrows):
+        if X['X1'][i] <= 4:
+            y.append(random.binomial(1, 0.5 + alpha))
+        else:
+            y.append(random.binomial(1, 0.5 - alpha))
+    y = Series(y)
     return X, y
 
 
 if __name__ == '__main__':
     MULTIPLE_EXPERIMENTS = True
     KFOLD = False
-    ONE_HOT = True
+    ONE_HOT = False
     COMPUTE_PERMUTATION = True
-    RESULTS_DIR = Path("5_is_connected_for_boxplot/")
+    RESULTS_DIR = Path("null/")
 
-    REGRESSION = True
+    REGRESSION = False
     x, y = get_x_y()
     contains_num_features = len(get_num_cols(x.dtypes)) > 0
     pp = get_preprocessing_pipeline if contains_num_features else get_preprocessing_pipeline_only_cat
     predictors = GBM_REGRESSORS if REGRESSION else GBM_CLASSIFIERS
 
-    config = Config(
-        multiple_experimens=MULTIPLE_EXPERIMENTS,
-        n_experiments=100,
-        kfold_flag=KFOLD,
-        compute_permutation=COMPUTE_PERMUTATION,
-        save_results=True,
-        one_hot=ONE_HOT,
-        contains_num_features=contains_num_features,
-        seed=SEED,
-        kfolds=KFOLDS,
-        predictors=predictors,
-        columns_to_remove=[],
-        get_x_y=get_x_y,
-        results_dir=RESULTS_DIR,
-        preprocessing_pipeline=pp)
-    experiment_configurator(config)
+    for results_dir, fun in {Path("null/"): partial(get_x_y, 0, True), Path("0.2/"): partial(get_x_y, 0.2, False)}.items():
+        config = Config(
+            multiple_experimens=MULTIPLE_EXPERIMENTS,
+            n_experiments=200,
+            kfold_flag=KFOLD,
+            compute_permutation=COMPUTE_PERMUTATION,
+            save_results=True,
+            one_hot=ONE_HOT,
+            contains_num_features=contains_num_features,
+            seed=SEED,
+            kfolds=KFOLDS,
+            predictors=predictors,
+            columns_to_remove=[],
+            get_x_y=fun,
+            results_dir=results_dir,
+            preprocessing_pipeline=pp)
+        experiment_configurator(config)
